3ae2ff369c86cb7a05f466f9eaeea955686afdb8
Tram <tramngo2@gmail.com>
2016-10-01 22:48:16 -1000

    completed

---
+++ app.js
@@ -0,0 +1,12 @@
+function change() {
+  var letters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++ ) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+   document.getElementById('posts').style.background = color;
+ }
+ 
+
+
+
